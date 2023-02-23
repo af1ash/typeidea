d885ebbd1af6001f94fb8cde45c1ce8d822ff665
lixiaobing <lixiaobing@memect.co>
2023-02-23 20:27:58 +0800

    [feat] 增加自定义过滤器，引入自定义css和js

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -26,30 +26,71 @@ class TagAdmin(admin.ModelAdmin):
         return super(TagAdmin, self).save_model(request, obj, form, change)
 
 
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """
+    自定义过滤器只展示当前用户的分类
+    """
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+    
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=category_id)
+        return queryset
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
 
-    list_display = ('title', 'category', 'status', 'created_time', 'operator')
+    list_display = ('title', 'category', 'owner', 'status', 'created_time', 'operator')
 
     list_display_links = []
 
-    list_filter = ['category']
+    list_filter = [
+        CategoryOwnerFilter
+    ]
     search_fields = ['title', 'category__name']
+    exclude = ('owner', )
 
     actions_on_top = True
     actions_on_bottom = True
 
+
     # 编辑界面
     save_on_top = True
 
-    fields = (
-        ('category', 'title'),
-        'desc',
-        'status',
-        'content',
-        'tag',
+    # fields = (
+    #     ('category', 'title'),
+    #     'desc',
+    #     'status',
+    #     'content',
+    #     'tag',
+    # )
+    fieldsets = (
+        ('基础配置', {
+            'description': '基础配置描述',
+            'fields': (
+                ('title', 'category'),
+                'status',
+            )
+        }),
+        ('内容', {
+            'fields': (
+                'desc',
+                'content'
+            ),
+        }),
+        ('额外信息', {
+            'classes': ('collapse', ),
+            'fields': ('tag', )
+        })
     )
+
+    filter_horizontal = ('tag', )
 
     def operator(self, obj):
         return format_html('<a href="{}">编辑</a>', reverse('admin:blog_post_change', args=(obj.id,)))
@@ -64,3 +105,15 @@ class PostAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change) -> None:
         obj.owner = request.user
         return super(PostAdmin, self).save_model(request, obj, form, change)
+    
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
+
+    # class Media:
+    #     css = {
+    #         'all': (
+    #             "https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css", 
+    #         ),
+    #     }
+    #     js = ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js", )
