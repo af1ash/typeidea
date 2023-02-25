a413c5420eaf3f56ddf01e1262683549cebb0aa0
lixiaobing <lixiaobing@memect.co>
2023-02-25 12:39:35 +0800

    [feat] 使用静态css

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -126,6 +126,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'themes', THEME, 'static')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
