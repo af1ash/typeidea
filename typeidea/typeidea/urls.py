c9988d4e030bb40b2eb2cce4331e3467531d38d3
lixiaobing <lixiaobing@memect.co>
2023-03-05 20:56:36 +0800

    [feat] 增加友链页面

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -21,7 +21,7 @@ from django.urls import re_path as url
 from blog.views import (
     IndexView, CategoryView, TagView, PostDetailView, SearchView, AuthorView
 )
-from config.views import links
+from config.views import LinkListView
 from .custom_site import custom_site
 
 urlpatterns = [
@@ -29,7 +29,7 @@ urlpatterns = [
     url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
     url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
     url(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view(), name='post-detail'),
-    url(r'^links/$', links, name='links'),
+    url(r'^links/$', LinkListView.as_view(), name='links'),
     url(r'^search/$', SearchView.as_view(), name='search'),
     url(r'^author/(?P<owner_id>\d+)/$', AuthorView.as_view(), name='author'),
     path('super_admin/', admin.site.urls, name='super-admin'),
