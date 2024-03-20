3b40c83384f47d2b1f156cc64f2a17e32fc0fced
lixiaobing <lixiaobing@memect.co>
2024-03-20 17:18:21 +0800

    [feat] 更新asgi配置

--- src/typeidea/asgi.py
+++ src/typeidea/asgi.py
@@ -9,8 +9,21 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'typeidea.settings')
 
-application = get_asgi_application()
+
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+from django.core.asgi import get_asgi_application
+from django.conf import settings
+
+profile = os.environ.get("EXTRACT_PROFILE", "develop")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"typeidea.settings.{profile}")
+
+# When in production you never, ever put gunicorn in front. 
+# Instead you use a server like nginx which dispatches requests 
+# to a pool of gunicorn workers and also serves the static files.
+if settings.WITH_STATIC:
+    application = StaticFilesHandler(get_asgi_application())
+else:
+    application = get_asgi_application()
